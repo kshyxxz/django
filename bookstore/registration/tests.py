42fc9d1218ac149c5c428a0d6aa77f21d5ed6928
kshyxxz <kshyxxz@gmail.com>
2026-02-16 12:03:52 +0545

    djangoDay8_2_GithubActions

--- bookstore/registration/tests.py
+++ bookstore/registration/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .utils import add
+from django.urls import reverse
 
-# Create your tests here.
+class AddFunctionTestCase(TestCase):
+	def test_add(self):
+		self.assertEqual(add(2,3),5)
+
+class RegistrationFormPageTestCase(TestCase):
+	def test_registration_contains_input_text(self):
+		response = self.client.get(reverse('registration:form'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, 'Name')
+		self.assertContains(response, 'Email')
+		self.assertContains(response, 'Password:')
