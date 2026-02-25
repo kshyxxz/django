9782223ff22da89839fcdcc0b790e3d760f853c5
kshyxxz <kshyxxz@gmail.com>
2026-02-25 09:49:26 +0545

    djangoDay10_SQLinjection_CSRF

--- bookstore/registration/urls.py
+++ bookstore/registration/urls.py
@@ -5,5 +5,6 @@ app_name = 'registration'
 
 urlpatterns = [
 	path('form/',views.registration_form, name='form'),
+	path('form/sql/',views.registration_form_sql_injection, name='form_sql'),
 	path('',views.list_user, name='list'),
 ]
--- bookstore/registration/views.py
+++ bookstore/registration/views.py
@@ -2,11 +2,37 @@ from django.shortcuts import render,redirect
 from .forms import RegistrationForm
 from .models import Registration
 from django.contrib.auth.hashers import make_password
+from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
 
 def list_user(request):
 	users = Registration.objects.all()
 	user_input = "this is user inptut"
 	return render(request, 'registration/list.html', {'users': users, 'user_input': user_input})
+
+@csrf_exempt
+def registration_form_sql_injection(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+
+		if form.is_valid():
+			data = form.cleaned_data
+			hashPass = make_password(data['password'])
+			
+			sql = f"INSERT INTO registration_registration (name, email, password) VALUES ('{data['name']}', '{data['email']}', '{hashPass}')"
+			with connection.cursor() as cursor:
+				cursor.execute(sql)
+			print("Success")
+
+			return render(request, 'registration/form.html', {
+				'form': RegistrationForm(),
+				'success': "Successful"
+			})
+		
+	else:
+		form = RegistrationForm()
+
+	return render(request, 'registration/form.html', {'form': form})
 
 def registration_form(request):
 	if request.method == 'POST':
